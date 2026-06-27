b1932c32ca29cffcca7d59b245aadf9298a028db
Rahul Ghosh <rahul.info305@gmail.com>
2026-06-27 18:38:04 +0600

    delete dist

--- dist/routes/guest.route.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const guest_controller_1 = require("src/controllers/guest.controller");
-const router = (0, express_1.Router)();
-router.get("/", guest_controller_1.baseUrl);
-exports.default = router;
